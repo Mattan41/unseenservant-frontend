56200583acbefb8f0817f5c3ad59f3ba87979c5b
Mats Kruskopf Eriksson <knishopf@gmail.com>
2026-04-26 17:01:22 +0200

    update characterImage

--- src/features/character/components/CharacterImage.vue
+++ src/features/character/components/CharacterImage.vue
@@ -15,15 +15,13 @@ const props = defineProps({
     src.startsWith('https://') ||
     src.startsWith('blob:');
 
-  const fullSrc = computed(() => {
-    if (!props.src) return props.defaultSrc;
-    if (isAbsolute(props.src)) return props.src;
-    if (import.meta.env.DEV) {
-      // In dev: prefix with api-url if you are using a relative path
-      return `${import.meta.env.VITE_API_BASE_URL}${props.src}`;
-    }
-    // In prod: relativ paths (or nginx should proxy)
-    return props.src;
+const fullSrc = computed(() => {
+  if (!props.src) return props.defaultSrc;
+  if (isAbsolute(props.src)) return props.src;
+  if (import.meta.env.DEV && props.src.startsWith('/images/')) {
+    return `${import.meta.env.VITE_API_BASE_URL}${props.src}`;
+  }
+  return props.src;
 });
 
 </script>
